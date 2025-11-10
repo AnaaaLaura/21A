d9c9bb8d99813e65bbe2dcb990fe50febc0a7014
AnaaaLaura <162182134+AnaaaLaura@users.noreply.github.com>
2025-11-10 20:55:34 -0300

    TEHNTANDODENOCOP7

--- buscarAlunosMagll.js
+++ buscarAlunosMagll.js
@@ -1,11 +1,11 @@
-async function listarTodos() {
+async function ListarAlunos() {
     const buscaNoBancoDeDados = await fetch('http://localhost:3000/alunosMagll');
     const respostaObtida = await buscaNoBancoDeDados.json();
     console.log(respostaObtida);
     let html = '<table border="1"><tr><th>CPF</th><th>Nome</th></tr>';
 
     respostaObtida.forEach(alunosMagll => {
-        html += `<tr><td>${alunosMagll.id}</td><td>${alunosMagll.nome}</td><td>${alunosMagll.email}</td><td>${alunosMagll.idade}</td><td>${alunosMagll.peso}</td><td>${alunosMagll.objetivo}</td></tr>`;
+        html += `<tr><td>${alunosMagll.id}</td><td>${alunosMaglls.nome}</td><td>${alunosMagll.email}</td><td>${alunosMagll.idade}</td><td>${alunosMagll.peso}</td><td>${alunosMagll.objetivo}</td></tr>`;
     });
 
     html += '</table>';
